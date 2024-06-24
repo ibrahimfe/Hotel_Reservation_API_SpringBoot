2c6373650cb9fa57eb52f82c9a5351ab6863d931
ibrahimfe <ibrahiimm.f@gmail.com>
2024-06-24 12:39:38 +0700

    initial commit

---
+++ src/main/java/api/hotel_reservation/HotelApi/HotelApiApplication.java
@@ -0,0 +1,22 @@
+package api.hotel_reservation.HotelApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HotelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelApiApplication.class, args);
+
+	}
+
+	@GetMapping("/")
+	public String apiRoot() {
+		return "Hello, World";
+	}
+
+}
---
+++ src/test/java/api/hotel_reservation/HotelApi/HotelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.hotel_reservation.HotelApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
